315ad18b1768d85617ecad22b9f95a7285992418
adrianmt <adrian@multitracks.com>
2020-06-22 10:31:10 -0500

    completed course

---
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail'
+  product: IProduct;
+  errorMessage: string;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {
+      
+
+  }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+
+    this.productService.getProduct(id).subscribe({
+      next: product => {
+        this.product = product;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+}
